09fa944ccc407956b79e0c989c91f53d2402412b
theorm <theorm@gmail.com>
2014-03-25 13:19:36 +1000

    - added blockchain

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var nytimes = require('./lib/nytimes')
-  , guardian = require('./lib/guardian');
+  , guardian = require('./lib/guardian')
+  , blockchain = require('./lib/blockchain');
 
 module.exports.NYTimes = nytimes.NYTimes;
 module.exports.Guardian = guardian.Guardian;
+module.exports.Blockchain = blockchain.Blockchain;
